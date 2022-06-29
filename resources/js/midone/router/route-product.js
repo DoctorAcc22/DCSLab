7a49d4310d207e0c8867b2b6a874f342c0759b5a
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2022-06-29 16:20:22 +0700

    sad

--- resources/js/midone/router/route-product.js
+++ resources/js/midone/router/route-product.js
@@ -1,40 +1,50 @@
 import SideMenu from "@/layouts/side-menu/Main.vue";
 import ProductGroup from "@/views/product_group/ProductGroup.vue";
+import Brand from "@/views/brand/Brand.vue";
 import Product from "@/views/product/Product.vue";
 import Service from "@/views/product/Service.vue";
 
-const root = '/dashboard';
+const root = "/dashboard";
 
 export default {
-    path: root + '/product',
+    path: root + "/product",
     component: SideMenu,
     children: [
         {
-            path: root + '/product' + '/product_group',
-            name: 'side-menu-product-product_group',
+            path: root + "/product" + "/product_group",
+            name: "side-menu-product-product_group",
             component: ProductGroup,
-            meta: { 
+            meta: {
                 remember: true,
-                log_route: true
-            }
+                log_route: true,
+            },
         },
         {
-            path: root + '/product' + '/product',
-            name: 'side-menu-product-product',
+            path: root + "/product" + "/brand",
+            name: "side-menu-product-brand",
+            component: Brand,
+            meta: {
+                remember: true,
+                log_route: true,
+            },
+        },
+        {
+            path: root + "/product" + "/product",
+            name: "side-menu-product-product",
             component: Product,
-            meta: { 
+            meta: {
                 remember: true,
-                log_route: true
-            }
+                log_route: true,
+            },
         },
         {
-            path: root + '/product' + '/service',
-            name: 'side-menu-product-service',
+            path: root + "/product" + "/service",
+            name: "side-menu-product-service",
             component: Service,
-            meta: { 
+            meta: {
                 remember: true,
-                log_route: true 
-            }
-        }
-    ]
+                log_route: true,
+            },
+        },
+    ],
 };
